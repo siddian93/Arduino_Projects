a8403520977edd4fe7b8b1ffbe55e8aa6ceabd9d
siddian93 <siddian.narayana@gmail.com>
2018-02-26 02:27:29 +0530

    Projects on Arduino Added

---
+++ Remote Temperature Sensing/Temp_Ethernet_V1.0.ino
@@ -0,0 +1,126 @@
+/*
+  Web Server
+
+ A simple web server that shows the value of the analog input pins.
+ using an Arduino Wiznet Ethernet shield.
+
+ Circuit:
+ * Ethernet shield attached to pins 10, 11, 12, 13
+ * Analog inputs attached to pins A0 through A5 (optional)
+
+ created 18 Dec 2009
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include<DallasTemperature.h>
+#include<OneWire.h>
+
+#define ONE_WIRE_BUS 2
+#define buzzer 3
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+OneWire oneWire(ONE_WIRE_BUS);
+float temp=0.00;
+DallasTemperature sensors(&oneWire);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  sensors.begin();
+  pinMode(buzzer, OUTPUT);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  sensors.requestTemperatures();
+  temp=sensors.getTempCByIndex(0);
+  Serial.print(temp);
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/xml");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+          client.println();
+          client.println("<sensordata>");
+          //client.println("<!DOCTYPE HTML>");
+          
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print(" Temerature at Room : ");
+            client.println("<data>");
+            client.print(temp);
+            client.println("</data>");
+            
+          }
+          client.println("</sensordata>");
+          //client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+    if(temp>30){
+      digitalWrite(buzzer, HIGH);
+    }
+    else{ 
+      digitalWrite(buzzer, LOW);
+    }
+  }
+  //digitalWrite(buzzer, HIGH);
+}
+
